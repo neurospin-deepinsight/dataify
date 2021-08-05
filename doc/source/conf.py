2620997ac99decf5179a168f905ec078e41b1791
Antoine Grigis <antoine.grigis@cea.fr>
2021-08-05 17:42:18 +0200

    doc/source/conf: allow cross refs.

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -75,7 +75,8 @@ sphinx_gallery_conf = {
     "doc_module": "dataify",
     "backreferences_dir": os.path.join("dataify", "gallery"),
     "examples_dirs": os.path.join(os.pardir, "examples"),
-    "gallery_dirs": "auto_gallery"}
+    "gallery_dirs": "auto_gallery",
+    "reference_url": {"dataify": None}}
 
 # Remove some numpy-linked warnings
 numpydoc_show_class_members = False
